d36c8365ad26e874fa342b5abe8ee365db43845e
cyproto <kyashkarande@gmail.com>
2020-06-24 00:38:02 +0530

    meals_app: Remove scaffold and return GridView.

--- meals_app/lib/screens/categories_screen.dart
+++ meals_app/lib/screens/categories_screen.dart
@@ -6,11 +6,7 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals App'),
-      ),
-      body: GridView(
+    return GridView(
         padding: EdgeInsets.all(15),
         children: DUMMY_CATEGORIES
             .map((categoryData) =>
@@ -21,7 +17,6 @@ class CategoriesScreen extends StatelessWidget {
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10),
-      ),
     );
   }
 }
