6e8340628a8b705f1e2407d7a637c7407722d055
cyproto <kyashkarande@gmail.com>
2020-07-18 21:22:44 +0530

    great_places_app: Initial commit.

---
+++ great_places_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.redAccent,
+      ),
+    );
+  }
+}
