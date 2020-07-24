3c8a0549e6753b853047b51af4e15b7ece0eda7c
cyproto <kyashkarande@gmail.com>
2020-07-24 22:38:55 +0530

    chat_app: Initial commit.

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
---
+++ chat_app/lib/screens/chat_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('chats'),
+        ),
+      ),
+    );
+  }
+}
