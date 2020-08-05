ce522c5123146f02967eb6f56c681c3b2704aabb
cyproto <kyashkarande@gmail.com>
2020-08-05 23:40:06 +0100

    chat_app: Add image picker and use it in signup page.

--- chat_app/lib/widgets/auth/auth_form.dart
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../pickers/user_image_picker.dart';
+
 class AuthForm extends StatefulWidget {
   final void Function(
     String email,
@@ -49,6 +51,7 @@ class _AuthFormState extends State<AuthForm> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
+                if (!_isLogin) UserImagePicker(),
                 TextFormField(
                   key: ValueKey('email'),
                   validator: (value) {
---
+++ chat_app/lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+    final pickedImageFile = File(pickedImage.path);
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 60,
+          backgroundColor: Color(0xffdadada),
+          backgroundImage: _pickedImage != null
+              ? FileImage(
+                  _pickedImage,
+                )
+              : null,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        FlatButton.icon(
+          color: Theme.of(context).accentColor,
+          onPressed: _pickImage,
+          icon: Icon(
+            Icons.image,
+          ),
+          label: Text('Add image'),
+        ),
+      ],
+    );
+  }
+}
